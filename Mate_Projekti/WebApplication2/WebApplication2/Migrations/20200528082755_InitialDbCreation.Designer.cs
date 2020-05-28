50b2b5dd6d89e9bf5112e14607795290841e02c9
Nikola <nsricaz@racunarstvo.hr>
2020-05-28 10:35:03 +0200

    LocalDB GoogleMaps Working

--- Mate_Projekti/WebApplication2/WebApplication2/Migrations/20200528082755_InitialDbCreation.Designer.cs
+++ Mate_Projekti/WebApplication2/WebApplication2/Migrations/20200528082755_InitialDbCreation.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApplication2.Models;
 
 namespace WebApplication2.Migrations
 {
     [DbContext(typeof(Google_mapsContext))]
-    partial class Google_mapsContextModelSnapshot : ModelSnapshot
+    [Migration("20200528082755_InitialDbCreation")]
+    partial class InitialDbCreation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
